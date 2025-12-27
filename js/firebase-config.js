fe58d9fc484b9f2b5aa10e46cda357da80ed2a4c
NelSystems <nelsystems77@gmail.com>
2025-12-27 10:55:37 -0600

    Fix firebase-config.js initialization and export db

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,10 +10,11 @@ const firebaseConfig = {
     storageBucket: "mediconsult-pwa.firebasestorage.app",
     messagingSenderId: "572003898944",
     appId: "1:572003898944:web:d5c606f7b2491a48cfb1a5",
-    measurementId: "G-8XRT85SW4R"
-    appId: "1:572003898944:web:d5c606f7b2491a48cfb1a5"
+    measurementId: "G-8XRT85SW4R",
+    appId: "1:572003898944:web:d5c606f7b2491a48cfb1a5",
 };
 
 // Inicializamos la app y la base de datos
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
